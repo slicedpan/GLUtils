ffa843948d27950516b42ea55139099f913de7cf
Owen Mooney <omooney@tcd.ie>
2012-04-02 02:04:30 +0100

    added more colour utilities, fixed bugs in vbomesh

--- Colours.h
+++ Colours.h
@@ -3,39 +3,64 @@
 class Colours
 {
 public:
-	static const Vec3& Red() { return instance->red; }
-	static const Vec3& Blue() { return instance->blue; }
-	static const Vec3& Green() { return instance->green; }
-	static const Vec3& White() { return instance->white; }
-	static const Vec3& Black() { return instance->black; }
-	static const Vec3& Purple() { return instance->purple; }
-	static const Vec3& Yellow() { return instance->yellow; }
-	static const Vec3& Cyan() { return instance->cyan; }
+	static const Vec4& Red() { return instance->red; }
+	static const Vec4& Blue() { return instance->blue; }
+	static const Vec4& Green() { return instance->green; }
+	static const Vec4& White() { return instance->white; }
+	static const Vec4& Black() { return instance->black; }
+	static const Vec4& Purple() { return instance->purple; }
+	static const Vec4& Yellow() { return instance->yellow; }
+	static const Vec4& Cyan() { return instance->cyan; }
 	Colours()
 	{
-		red = Vec3(1.0f, 0.0f, 0.0f);
-		blue = Vec3(0.0f, 0.0f, 1.0f);
-		white = Vec3(1.0f, 1.0f, 1.0f);
-		black = Vec3(0.0f, 0.0f, 0.0f);
-		green = Vec3(0.0f, 1.0f, 0.0f);
-		purple = Vec3(1.0f, 0.0f, 1.0f);
-		yellow = Vec3(1.0f, 1.0f, 0.0f);
-		cyan = Vec3(0.0f, 1.0f, 1.0f);
+		red = Vec4(1.0f, 0.0f, 0.0f, 1.0f);
+		blue = Vec4(0.0f, 0.0f, 1.0f, 1.0f);
+		white = Vec4(1.0f, 1.0f, 1.0f, 1.0f);
+		black = Vec4(0.0f, 0.0f, 0.0f, 1.0f);
+		green = Vec4(0.0f, 1.0f, 0.0f, 1.0f);
+		purple = Vec4(1.0f, 0.0f, 1.0f, 1.0f);
+		yellow = Vec4(1.0f, 1.0f, 0.0f, 1.0f);
+		cyan = Vec4(0.0f, 1.0f, 1.0f, 1.0f);
 	}
-	static void Copy(float* arr, const Vec3& source)
+	static void Copy3(float* arr, const Vec3& source)
 	{
 		arr[0] = source[0];
 		arr[1] = source[1];
 		arr[2] = source[2];
 	}
+	static void Copy3(float* arr, const Vec4& source)
+	{
+		arr[0] = source[0];
+		arr[1] = source[1];
+		arr[2] = source[2];
+	}
+	static void Copy4(float* arr, const Vec4& source)
+	{
+		arr[0] = source[0];
+		arr[1] = source[1];
+		arr[2] = source[2];
+		arr[3] = source[3];
+	}
+	static Vec4 Lerp(Vec4 colour1, Vec4 colour2, float factor)
+	{
+		return Vec4(colour1 * factor + colour2 * (1.0f - factor));
+	}
+	static Vec3 Lerp(Vec3 colour1, Vec3 colour2, float factor)
+	{
+		return Vec3(colour1 * factor + colour2 * (1.0f - factor));
+	}
+	static Vec4 FromByte(unsigned char r, unsigned char g, unsigned char b, unsigned char a)
+	{
+		return Vec4(r / 255.0f, g / 255.0f, b / 255.0f, a / 255.0f);
+	}
 private:
 	static Colours* instance;
-	Vec3 red;
-	Vec3 blue;
-	Vec3 green;
-	Vec3 purple;
-	Vec3 yellow;
-	Vec3 black;
-	Vec3 white;
-	Vec3 cyan;
+	Vec4 red;
+	Vec4 blue;
+	Vec4 green;
+	Vec4 purple;
+	Vec4 yellow;
+	Vec4 black;
+	Vec4 white;
+	Vec4 cyan;
 };
